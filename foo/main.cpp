787ca25fc9c389e39af4175eadb677524d4cf1c0
bryan hunt <bryan.hunt@picsolve.com>
2013-11-27 13:20:08 +0000

    Initial Commit

---
+++ foo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  foo
+//
+//  Created by bryan hunt on 27/11/2013.
+//  Copyright (c) 2013 bryan hunt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
